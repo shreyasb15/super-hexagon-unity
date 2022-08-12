431c7d67ef001c8666b5c81afa4838d358cd8fb7
Shreyas Borakhadikar <shreyasb1511@gmail.com>
2022-08-12 14:27:08 +0530

    Minor changes

--- Super_Hexagon/Assets/Scripts/Rotator.cs
+++ Super_Hexagon/Assets/Scripts/Rotator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    public float cameraRotateSpeed = 30f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.forward, Time.deltaTime * 30f);
+        transform.Rotate(Vector3.forward, Time.deltaTime * cameraRotateSpeed);
     }
 }
